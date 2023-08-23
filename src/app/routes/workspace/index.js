a265f660dca61b73b082f46a9cb6de5499d6ad53
henrique <henriquecordeiro054@gmail.com>
2023-08-23 20:33:01 -0300

    add css workspace

--- src/app/routes/workspace/index.js
+++ src/app/routes/workspace/index.js
@@ -1,14 +1,17 @@
 'use client'
 
+import styles from './workspace.module.css';
 import BarIniatial from '../../components/BarIniatial';
 import VideoList from '../../components/VideoList';
-
+//teste
 export default function workspace() {
 
     return (
         <>
-            <BarIniatial />
-            <VideoList />
+            <div className={styles.main}>
+                <BarIniatial />
+                <VideoList />
+            </div>
         </>
     )
 
