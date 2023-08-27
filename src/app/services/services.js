0a53d12c395a98c952db9509d32971d1bd59bc3f
henrique <henriquecordeiro054@gmail.com>
2023-08-27 19:31:55 -0300

    add get and post twitch.tv

--- src/app/services/services.js
+++ src/app/services/services.js
@@ -1,23 +1,56 @@
-import axios from "axios"
+import axios from "axios";
 
-const TwitchApi = axios.create({baseURL: "http://localhost:8000/favoritos"})
-
-async function getFavoritos() {
-    const response = await favoritosAPI.get('/')
-
-    return response.data
+function geraToken(client_id,client_secret) {
+    try {
+        const response =  axios.post('https://id.twitch.tv/oauth2/token', {
+            client_id: client_id,
+            client_secret: client_secret,
+            grant_type: 'client_credentials'
+        })
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+  
 }
 
-async function postFavorito(id) {
-    await favoritosAPI.post(`/${id}`)
+
+function pegaStreamer(client_id,token,streamer) {
+    try {
+        const response =  axios.get(`https://api.twitch.tv/helix/users?login=${streamer}`, {
+            headers: {
+                'Client-Id': client_id,
+                'Authorization': token }
+        })
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+  
 }
 
-async function deleteFavorito(id) {
-    await favoritosAPI.delete(`/${id}`)
+function pegaClips(client_id,token,broadcaster_id,data) {
+    //formarto da data deve ser 2023-08-23T15:00:00Z
+    try {
+        const response =  axios.get(`https://api.twitch.tv/helix/clips?broadcaster_id=${broadcaster_id}&started_at=${data}'`, {
+            headers: {
+                'Client-Id': client_id,
+                'Authorization': token }
+        })
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
-export {
-    getFavoritos,
-    postFavorito,
-    deleteFavorito
+https://api.twitch.tv/helix/games/top
+
+module.exports = {
+    geraToken,
+    pegaStreamer,
+    pegaClips
 }
