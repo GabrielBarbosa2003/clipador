f1170d6ba8617c1b425b308cd841ef793c9a27ab
gabriel <gabrieldecarvalhu@gmail.com>
2023-08-31 21:58:17 -0300

    ADD: novas rotas e refatoracao barra lateral

---
+++ src/app/layouts/index.js
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Sidebar from "../components/BarIniatial"
+import styles from "./default.module.css"
+
+ function PaginaDefault() {
+    return (
+        <div className={styles.layout}>
+            <Sidebar/>
+            <div className={styles.content}>
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+export default PaginaDefault;
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,10 @@
 'use client'
 import styles from './page.module.css';
-import Workspace from './routes/workspace';
-import NotFound from './routes/Notfound';
-import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import AppRoutes from './routes';
 export default function Home() {
   return (
     <main className={styles.main}>
-
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Workspace />} />
-          <Route  path="/workspace" element={<Workspace />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-
+      <AppRoutes/>
     </main>
   )
 }
