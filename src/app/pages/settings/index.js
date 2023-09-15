3ff980639c40647f8edf540b27ce571b77434af0
euw3ll <wendrell.antoneli@gmail.com>
2023-09-15 15:02:21 -0300

    Rotas atualizadas

--- src/app/pages/settings/index.js
+++ src/app/pages/settings/index.js
@@ -1,7 +1,13 @@
+'use client'
+
+import styles from "./settings.module.css"
+
 export default function Settings() {
     return (
-        <div>
-            <h1>Settings</h1>
+        <div className={styles.main}>
+            <div className={styles.settings}>
+                <h1>Configurações</h1>
+            </div>
         </div>
     );
 }
