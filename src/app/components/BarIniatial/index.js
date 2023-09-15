9711975a8d443125f89d48a8d9115e44cd1265fc
euw3ll <wendrell.antoneli@gmail.com>
2023-09-15 02:03:05 -0300

    Mudanças nos icones da Sidebar

--- src/app/components/BarIniatial/index.js
+++ src/app/components/BarIniatial/index.js
@@ -2,8 +2,8 @@
 
 import { useState } from 'react'
 import styles from "./Navbar.module.css";
-import { FaUserCircle,FaEquals,FaEllipsisV } from 'react-icons/fa';
-import { FaAngleRight } from "react-icons/fa6";
+import { FaUserCircle, FaEquals, FaEllipsisV, FaQuestion } from 'react-icons/fa';
+import { FaAngleRight, FaGaugeHigh, FaEye, FaGear, FaRobot } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom';
 export default function Sidebar() {
 
@@ -23,8 +23,8 @@ export default function Sidebar() {
           <NavLink to="/"><img src='https://media.discordapp.net/attachments/1143986307253682328/1144089006171246632/LogoBRANCO.png?width=1025&height=284'/></NavLink>
           <div className={styles.Menu}>
             <ul className={styles.sidebar_list}>
-              <NavLink  to="/" className = {styles.link}> <FaAngleRight className={styles.arrowIcon} /> <h2>Dashboard</h2></NavLink>
-              <NavLink  className = {styles.link}> <FaAngleRight className={styles.arrowIcon} /> <h2>Monitoramento</h2></NavLink>
+              <NavLink  to="/" className = {styles.link}> <FaGaugeHigh className={styles.icons} /> <h2>Dashboard</h2></NavLink>
+              <NavLink  className = {styles.link}> <FaEye className={styles.icons} /> <h2>Monitoramento</h2></NavLink>
               <NavLink  to="/workspace" className = {styles.link}> <h4>Buscar clipes</h4></NavLink>
               <NavLink  to="/workspace" className = {styles.link}> <h4>Canais monitorados</h4></NavLink>
               <div>
@@ -36,8 +36,8 @@ export default function Sidebar() {
 
           <div className={styles.MenuFooter}>
             <ul className={styles.sidebar_list}>
-              <NavLink  to="/workspace" className ={styles.link}> <FaAngleRight className={styles.arrowIcon} /> <h2>Configuração</h2></NavLink>
-              <NavLink  to="/workspace" className ={styles.link}> <FaAngleRight className={styles.arrowIcon} /> <h2>Ajuda</h2></NavLink>
+              <NavLink  to="/workspace" className ={styles.link}> <FaGear className={styles.icons} /> <h2>Configuração</h2></NavLink>
+              <NavLink  to="/workspace" className ={styles.link}> <FaQuestion className={styles.icons} /> <h2>Ajuda</h2></NavLink>
             </ul>
 
             <div className={styles.border}></div>
